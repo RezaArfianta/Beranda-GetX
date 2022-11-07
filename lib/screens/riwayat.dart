7fd09200a74d55edb7510f4bf99bb7d0b59e9a18
hldashfii <hildashofiana1512@gmail.com>
2022-11-07 16:29:13 +0700

    fixed

--- lib/screens/riwayat.dart
+++ lib/screens/riwayat.dart
@@ -28,79 +28,82 @@ class GetXRiwayat extends StatelessWidget {
           ],
         ),
         body: Container(
-          child: Obx(() {
-            if (c.isLoading.value) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-
-            if (c.listRiwayat.value.isNotEmpty) {
-              return Container(
-                child: ScrollConfiguration(
-                    behavior: ScrollConfiguration.of(context)
-                        .copyWith(scrollbars: false),
-                    child: Column(
-                      children: <Widget>[
-                        Container(
-                          margin: EdgeInsets.all(15),
-                          height: 50,
-                          child: TextField(
-                            decoration: InputDecoration(
-                                prefixIcon: Icon(Icons.search),
-                                hintText: 'Judul Buku / Abstrak',
-                                hintStyle: TextStyle(fontSize: 15),
-                                border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(30),
-                                    borderSide:
-                                        BorderSide(color: Colors.white))),
-                            controller: c.InputController,
-                            onSubmitted: (text) {
-                              c.searchRiwayat();
-                            },
-                          ),
-                        ),
-                        Expanded(
-                            child: Container(
-                          child: ScrollConfiguration(
-                            behavior: ScrollConfiguration.of(context)
+          child: ScrollConfiguration(
+            behavior: ScrollConfiguration.of(context).copyWith(scrollbars: false),
+            child: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.all(15),
+                  height: 50,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.search),
+                      hintText: 'Judul Buku / Abstrak',
+                      hintStyle: TextStyle(fontSize: 15),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                        borderSide: BorderSide(color: Colors.white)
+                      )
+                    ),
+                    controller: c.InputController,
+                    onSubmitted: (text){
+                      c.searchRiwayat();
+                    },
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    child: ListView(
+                      controller: c.scrollcontroller.value,
+                      children: [
+                        Obx((){
+                          if(c.listRiwayat.value.isNotEmpty){
+                            return ScrollConfiguration(
+                              behavior: ScrollConfiguration.of(context)
                                 .copyWith(scrollbars: false),
-                            child: Obx(() => ListView.builder(
-                                  controller: c.scrollcontroller.value,
-                                  shrinkWrap: true,
-                                  padding: EdgeInsets.only(left: 10, right: 10),
-                                  itemCount: c.listRiwayat.value.length + 1,
-                                  itemBuilder: (context, int index) {
-                                    if (index == c.listRiwayat.length) {
-                                      return Padding(
-                                        padding:
-                                            EdgeInsets.symmetric(vertical: 20),
-                                        child: Center(
-                                          child: c.hasMore
-                                              ? const CircularProgressIndicator()
-                                              : const Text('Data habis'),
-                                        ),
-                                      );
-                                    } else {
-                                      return RiwayatCard(
-                                        iniRiwayat: c.listRiwayat.value[index],
-                                      );
-                                    }
-                                  },
-                                )),
-                          ),
-                        ))
+                            child: ListView.builder(
+                              shrinkWrap: true,
+                              padding: EdgeInsets.only(left: 10, right: 10),
+                              itemCount: c.listRiwayat.value.length + 1,
+                              itemBuilder: (context, int index) {
+                                if (index == c.listRiwayat.length) {
+                                  return Padding(
+                                    padding: EdgeInsets.symmetric(vertical: 20),
+                                    child: Center(
+                                      child: c.hasMore
+                                          ? Container()
+                                          : const Text('Data habis'),
+                                    ),
+                                  );
+                                } else {
+                                  return RiwayatCard(
+                                    iniRiwayat: c.listRiwayat.value[index],
+                                  );
+                                }
+                              },
+                            ),
+                            );
+                          }
+                          return Container();
+                        }),
+                        Obx(() {
+                        if (c.isLoading.value) {
+                          return Container(
+                            child: Center(
+                              child: CircularProgressIndicator(),
+                            ),
+                          );
+                        }
+                        return Container();
+                      }),
                       ],
-                    )),
-              );
-            }
-
-            return Container(
-              child: Center(
-                child: Text("Data tidak ditemukan"),
-              ),
-            );
-          }),
-        ));
+                      
+                    ),
+                  ),
+                )
+              ],
+            ),
+            ),
+     ) );
   }
 }
